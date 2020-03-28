fc195f3f2f91c99d546826a382ac67f227155f17
boringtao <xinghui.tao@wecorpindustries.com>
2020-03-28 01:52:49 +0000

    Important: vision_opencv

--- catkin_make.sh
+++ catkin_make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-catkin_make -DPYTHON_EXECUTABLE:FILEPATH=~/.virtualenvs/py3venv/bin/python
+catkin_make -DPYTHON_EXECUTABLE:FILEPATH=~/.virtualenvs/py3venv/bin/python3.6
--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -1,4 +1,4 @@
-#!/home/robotics/.virtualenvs/py3venv/bin/python
+#!/home/robotics/.virtualenvs/py3venv/bin/python3.6
 # -*- coding: utf-8 -*-
 
 # Software License Agreement (BSD License)
@@ -270,7 +270,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = '/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = '/home/robotics/Workspace/jetbot_dqn/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
