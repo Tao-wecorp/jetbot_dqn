b03f5075cc66a2c21580a6627e7e2c6ee9756165
boringtao <xinghui.tao@wecorpindustries.com>
2020-04-02 22:23:44 +0100

    Mod: check excution time

--- src/jetbot_dqn/scripts/pose_mobilenet.py
+++ src/jetbot_dqn/scripts/pose_mobilenet.py
@@ -2,67 +2,51 @@
 
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
+from copy import deepcopy
 import os
 import rospy
-import rospkg
-rospack = rospkg.RosPack()
 from sensor_msgs.msg import Image
+import time
 
-openpose_folder = os.path.join(rospack.get_path("jetbot_dqn"), "scripts/helpers/openpose/models/")
-net = cv2.dnn.readNetFromTensorflow(openpose_folder + "graph_opt.pb")
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+from helpers.openpose import OpenPose
+openpose = OpenPose()
 
-nPoints = 18
 class Pose(object):
     def __init__(self):
-        self.bridge_object = CvBridge()
+        rospy.init_node('pose_node', anonymous=True)
         self.image_sub = rospy.Subscriber("/robot/camera1/image_raw",Image,self.camera_callback)
+        self.bridge_object = CvBridge()
+        self.frame = None
+
+        rate = rospy.Rate(30)
+        while not rospy.is_shutdown():
+            if self.frame is not None:
+                start_time = time.time()
+                frame = deepcopy(self.frame)
+                # detect hip
+                points = openpose.detect(frame)
+                x, y = points[11]
+                # show hip point
+                frame = cv2.circle(frame, (int(x), int(y)), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+                cv2.imshow("", frame)
+                cv2.waitKey(1)
+                # check excution time
+                print("%s seconds" % (time.time() - start_time))
+            rate.sleep()
+            
     
     def camera_callback(self,data):
         try:
             cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
         except CvBridgeError as e:
             print(e)
-
-        frameWidth = cv_image.shape[1]
-        frameHeight = cv_image.shape[0]
-
-        inHeight = 368
-        inWidth = int((inHeight/frameHeight)*frameWidth)
-        net.setInput(cv2.dnn.blobFromImage(cv_image, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
-        output = net.forward()
-        output = output[:, :nPoints, :, :]
-
-        H = output.shape[2]
-        W = output.shape[3]
-
-        points = []
-        threshold = 0.1
-        for i in range(nPoints):
-            probMap = output[0, i, :, :]
-            minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-            x = (frameWidth * point[0]) / W
-            y = (frameHeight * point[1]) / H
-
-            if (prob > threshold):
-                cv2.circle(cv_image, (int(x), int(y)), 3, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-                # cv2.putText(cv_image, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3, lineType=cv2.LINE_AA)
-                points.append((int(x), int(y)))
-            else :
-                points.append(None)
-        print(points[11])
-        cv2.imshow("Keypoints",cv_image)
-        cv2.waitKey(1)
+        self.frame = cv_image
 
 def main():
-    rospy.init_node('pose_node', anonymous=True)
-    pose = Pose()
-    
     try:
-        rospy.spin()
+        Pose()
     except KeyboardInterrupt:
-        rospy.loginfo("Shutting down")
+        pass
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
