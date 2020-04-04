ce16e556805046feb47b1679529c0a7ed5ec40ce
boringtao <xinghui.tao@wecorpindustries.com>
2020-04-04 03:12:44 +0100

    Add: calcReward

--- src/jetbot_dqn/scripts/helpers/qlearning/objects.py
+++ src/jetbot_dqn/scripts/helpers/qlearning/objects.py
@@ -9,12 +9,24 @@ reward = 0.0
 
 class QLearning():
     def __init__(self):
-        self.state = 0.0  # [angle]
+        self.state = -10.0  # [angle]
         self.position = [0.0, 0.0]  # [x, y]
         self.goal = 0.0  # [angle]
-        self.angle2goal = 0.0  # [angle]
+        self.full_angel = 0.0  # [angle]
         self.reward = 0.0  # point
 
-    def yaw(self, point):
-        goal = degrees(atan((point[0]-320)/(480-point[1])))
-        return goal
+    def setState(self, state):
+        self.state = state
+    
+    def setGoal(self, position):
+        self.goal = degrees(atan((position[0]-320)/(480-position[1])))
+        self.full_angel = 2 * degrees(atan(320/(480-position[1])))
+       
+    def calcReward(self):
+        self.reward  = (1-abs(self.goal-self.state)/self.full_angel)*100
+
+    def yaw(self, position):
+        self.goal = degrees(atan((position[0]-320)/(480-position[1])))
+        self.full_angel = 2 * degrees(atan(320/(480-position[1])))
+        self.reward = (1-abs(self.goal-self.state)/self.full_angel)*100
+        return self.state, self.goal, self.reward
