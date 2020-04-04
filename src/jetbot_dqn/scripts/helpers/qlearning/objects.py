4d1dd76870dc97eb93134326aacbfda791d704a6
boringtao <xinghui.tao@wecorpindustries.com>
2020-04-04 03:27:20 +0100

    Add: step

--- src/jetbot_dqn/scripts/helpers/qlearning/objects.py
+++ src/jetbot_dqn/scripts/helpers/qlearning/objects.py
@@ -30,3 +30,14 @@ class QLearning():
         self.full_angel = 2 * degrees(atan(320/(480-position[1])))
         self.reward = (1-abs(self.goal-self.state)/self.full_angel)*100
         return self.state, self.goal, self.reward
+    
+    def step(self, act, learning_rate):
+        new_state = self.state + (act * learning_rate)
+
+        new_state = max(new_state, self.full_angel)
+        new_state = min(new_state, self.full_angel*(-1))
+
+        self.setState(new_state)
+        reward = self.calcReward()
+
+        return self.state, reward
